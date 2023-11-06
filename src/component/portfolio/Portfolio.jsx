45a29f0559654c4c6517e49ca96517c3ef3e21e2
kexinsee99 <seekexin99@gmail.com>
2023-11-06 18:58:32 +0800

    added project ArtHome

--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -1,44 +1,54 @@
 import React from 'react'
 import './portfolio.css'
-import PP from '../../assets/personal-portfolio1.png'
-import SG from '../../assets/silent-gestures1.png'
-import VA from '../../assets/viscene-admin-User73Video1-edited.png'
-import CELS from '../../assets/CELS.png'
+import PersonalPortfolio from '../../assets/project-images/personal-portfolio1.png'
+import SilentGestures from '../../assets/project-images/silent-gestures1.png'
+import VisceneAdmin from '../../assets/project-images/viscene-admin-User73Video1-edited.png'
+import ArtHome from '../../assets/project-images/arthome3.jpg'
+import CELS from '../../assets/project-images/CELS.png'
 
 const data = [
   {
     id: 1,
-    image: PP,
+    image: PersonalPortfolio,
     title: "Personal Portfolio",
     github: "https://github.com/kexinsee99/react-portfolio-website",
-    demo: "https://dribbble.com/shots/22652908-Personal-Portfolio",
+    screenshot: "https://dribbble.com/shots/22652908-Personal-Portfolio",
     language: "React JS, CSS",
     year: 2023
   },
   {
     id: 2,
-    image: SG,
+    image: SilentGestures,
     title: "Silent Gestures",
     github: "https://github.com/kexinsee99/translate-android",
-    demo: "https://dribbble.com/shots/22635758-Silent-Gestures",
+    screenshot: "https://dribbble.com/shots/22635758-Silent-Gestures",
     language: "Kotlin",
     year: 2023
   },
   {
     id: 3,
-    image: VA,
+    image: VisceneAdmin,
     title: "Viscene Admin",
     github: "",
-    demo: "https://dribbble.com/shots/22630119-Viscene-Admin",
+    screenshot: "https://dribbble.com/shots/22630119-Viscene-Admin",
     language: "TypeScript",
     year: 2022
   },
   {
     id: 4,
+    image: ArtHome,
+    title: "ArtHome (Design Prototype)",
+    github: "",
+    screenshot: "https://dribbble.com/shots/22986862-ArtHome",
+    language: "Figma, Adobe Premiere Pro",
+    year: 2020
+  },
+  {
+    id: 5,
     image: CELS,
     title: "Celebrate Every Little Success",
     github: "",
-    demo: "https://dribbble.com/shots/22629222-Celebrate-Every-Little-Success",
+    screenshot: "https://dribbble.com/shots/22629222-Celebrate-Every-Little-Success",
     language: "HTML, CSS, jQuery, PHP, Cordova",
     year: 2019
   }
@@ -52,7 +62,7 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         {
-          data.map(({id, image, title, github, demo, language, year}) => {
+          data.map(({id, image, title, github, screenshot, language, year}) => {
             let disabledClass = '';
             if (!github) {
               disabledClass = 'disabled';
@@ -69,7 +79,7 @@ const Portfolio = () => {
                 </h3>
                 <div className="portfolio__item-cta">
                   <a href={github} className={`btn ${disabledClass}`}  target='_blank'>Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                  <a href={screenshot} className='btn btn-primary' target='_blank'>Screenshots</a>
                 </div>
               </article>
             )
